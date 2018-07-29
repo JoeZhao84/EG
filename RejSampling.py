51e00f2a6d6280fab544de87232ca8c6d6f46922
JoeZhao84 <JoeZhao84@users.noreply.github.com>
2018-07-29 12:44:04 +0100

    Update RejSampling.py

--- RejSampling.py
+++ RejSampling.py
@@ -21,32 +21,45 @@ df.dtypes
 df.groupby(['Pclass', 'Sex', 'Embarked']).size()
 
 #so the biggest difference is male/S/3: 265 and female/s/3: 88
-ratio = 88 / 265    #0.33..
+#ratio = 88 / 265    #0.33..
 
 Pclass_list = df.Pclass.unique()
 Embarked_list = df.Embarked.unique()
 Embarked_list = Embarked_list[~pd.isnull(Embarked_list)]
 #Sex_list = df.Sex.unique()
 
-newDF = pd.DataFrame() 
-for a in Pclass_list:
-    for b in Embarked_list:
-        male_count = df[(df['Pclass'] == a) & (df['Embarked'] == b) & (df['Sex'] == 'male')]['Sex'].count()
-        female_count_e = int(round(male_count * ratio))
-    
-        acc_sample = df[(df['Pclass'] == a) & (df['Embarked'] == b) & (df['Sex'] == 'female')].sample(n=female_count_e,replace=True)
-    
-        
-        newDF = newDF.append(acc_sample, ignore_index=True)   
+def get_ratio(df):
+    ratios = []
+    for a in Pclass_list:
+        for b in Embarked_list:
+            male_count = df[(df['Pclass'] == a) & (df['Embarked'] == b) & (df['Sex'] == 'male')]['Sex'].count()
+            female_count = df[(df['Pclass'] == a) & (df['Embarked'] == b) & (df['Sex'] == 'female')]['Sex'].count()
+            ratio = female_count / male_count
+            ratios.append(ratio)
+            min_ratio = min(ratios)
+    return min_ratio
 
+ratio =  get_ratio(df)           
+                
+def get_RejSample(df):
+    newDF = pd.DataFrame() 
+    for a in Pclass_list:
+        for b in Embarked_list:
+            male_count = df[(df['Pclass'] == a) & (df['Embarked'] == b) & (df['Sex'] == 'male')]['Sex'].count()
+            female_count_e = int(round(male_count * ratio))
+            acc_sample = df[(df['Pclass'] == a) & (df['Embarked'] == b) & (df['Sex'] == 'female')].sample(n=female_count_e,replace=True)
+            newDF = newDF.append(acc_sample, ignore_index=True)   
+    return newDF
     
 #test
-newDF[(newDF['Pclass'] == 1) & (newDF['Embarked'] == 'Q') & (newDF['Sex'] == 'female')].count()
+sample = get_RejSample(df)
+#newDF[(newDF['Pclass'] == 1) & (newDF['Embarked'] == 'C') & (newDF['Sex'] == 'female')].count()
+sample[(sample['Pclass'] == 1) & (sample['Embarked'] == 'S') & (sample['Sex'] == 'female')].count()
+   
     
     
     
-    
-#phototype   
+   
 male_count = df[(df['Pclass'] == 1) & (df['Embarked'] == 'C') & (df['Sex'] == 'male')]['Sex'].count()
 female_count_e = int(round(male_count * ratio))
 
